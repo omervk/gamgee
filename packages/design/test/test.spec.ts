90b07329408bac7e49d8dca079c777be7923acd9
Omer Van kloeten <omer.van.kloeten@forter.com>
2024-02-22 19:43:16 +0200

    Added actual (regression) tests for @gamgee/design

--- packages/design/test/test.spec.ts
+++ packages/design/test/test.spec.ts
@@ -1,29 +1,21 @@
 import { mermaidToScaffold } from '../src/scaffold'
+import { readFileSync } from 'node:fs'
+import { expect } from '@jest/globals'
 
-const fileText: string = `
----
-title: Example Workflow
----
+describe('test generated scaffolds', () => {
+    const tests = ['simple-workflow', 'conditions']
 
-stateDiagram-v2
-    direction LR
-
-    state POST_FIRST_TASK <<choice>>
-
-    [*] --> first_task
-    first_task --> first_task: attempts=3
-    first_task --> POST_FIRST_TASK
-    POST_FIRST_TASK --> SecondTask: Value Is Null
-    SecondTask --> SecondTask: attempts=2\\nbackoffMs=2000
-    POST_FIRST_TASK --> thirdTask: Value Isn't Null
-    SecondTask --> thirdTask
-    thirdTask --> [*]
-`
-
-describe('test', () => {
-    it('test', () => {
-        const { generatedFilePath, contents } = mermaidToScaffold(fileText, './my-file.ts', '.')
-        console.log(generatedFilePath)
-        console.log(contents)
+    tests.forEach(testName => {
+        it.concurrent(`test ${testName}.mermaid`, () => {
+            const { generatedFilePath, contents } = mermaidToScaffold(
+                readFileSync(`./test/resources/${testName}.mermaid`, { encoding: 'utf8' }),
+                `./${testName}.ts`,
+                '.',
+            )
+            expect(generatedFilePath).toStrictEqual(`${testName}.generated.ts`)
+            expect(contents).toStrictEqual(
+                readFileSync(`./test/resources/${testName}.generated.ts`, { encoding: 'utf8' }),
+            )
+        })
     })
 })
