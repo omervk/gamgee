d9e3c25f4dec33029f09c12cc947ad795e93f5a5
Omer van Kloeten <omer.van.kloeten@gmail.com>
2024-02-25 21:11:14 +0200

    eslint

--- packages/design/src/diagram/parser.d.ts
+++ packages/design/src/diagram/parser.d.ts
@@ -1,25 +1,30 @@
-export type DirStatement = { stmt: 'dir', value: 'TB' | 'BT' | 'LR' | 'RL' }
+export type DirStatement = { stmt: 'dir'; value: 'TB' | 'BT' | 'LR' | 'RL' }
 export type StateStatement = {
-    stmt: 'state',
-    id: string,
-    type: 'default' | 'choice' | 'fork' | 'join' | 'divider',
-    classes?: string[],
-    description?: string,
-    doc?: string,
-    note?: { position: string, text: string }
+    stmt: 'state'
+    id: string
+    type: 'default' | 'choice' | 'fork' | 'join' | 'divider'
+    classes?: string[]
+    description?: string
+    doc?: string
+    note?: { position: string; text: string }
 }
-export type RelationStatement = { stmt: 'relation', state1: { id: string }, state2: { id: string }, description?: string }
-export type ClassDefStatement = { stmt: 'classDef', id: string, classes: string[] }
-export type ApplyClassStatement = { stmt: 'applyClass', id: string, styleClass: string }
+export type RelationStatement = {
+    stmt: 'relation'
+    state1: { id: string }
+    state2: { id: string }
+    description?: string
+}
+export type ClassDefStatement = { stmt: 'classDef'; id: string; classes: string[] }
+export type ApplyClassStatement = { stmt: 'applyClass'; id: string; styleClass: string }
 export type Statement = DirStatement | StateStatement | RelationStatement | ClassDefStatement | ApplyClassStatement
 
 export interface YY {
-    getDividerId: () => string;
-    setAccTitle: (string) => void;
-    setAccDescription: (string) => void;
-    setDirection: (string) => void;
-    setRootDoc: (string) => void;
-    trimColon: (string) => string;
+    getDividerId: () => string
+    setAccTitle: (string) => void
+    setAccDescription: (string) => void
+    setDirection: (string) => void
+    setRootDoc: (string) => void
+    trimColon: (string) => string
 }
 
 export const parser: {
